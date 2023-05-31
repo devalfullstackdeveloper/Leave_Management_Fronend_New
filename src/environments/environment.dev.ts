e8256158086684c8561a7ac374cab5089b5c39cd
amitkayasth1105 <amit@itidoltechnologies.com>
2023-05-31 18:04:19 +0530

    change

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     baseUrl: "http://103.204.189.43:3001/api/",
-    pdf_url: "http://103.204.189.43/leave-management-dev",
+    pdf_url: "http://103.204.189.43/leave-management-new",
     docUrl:  "http://103.204.189.43:3001/"
   };
   
