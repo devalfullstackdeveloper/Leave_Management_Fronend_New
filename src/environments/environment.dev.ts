b1669f34e378b6a59287c8602be84695bc94458c
amitkayasth1105 <amit@itidoltechnologies.com>
2023-06-02 11:14:11 +0530

    First Commit

--- src/environments/environment.dev.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: false,
-    baseUrl: "http://103.204.189.43:3001/api/",
-    pdf_url: "http://103.204.189.43/leave-management-new",
-    docUrl:  "http://103.204.189.43:3001/"
-  };
-  
